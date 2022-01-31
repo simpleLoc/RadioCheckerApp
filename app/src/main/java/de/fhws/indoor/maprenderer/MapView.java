62c11d1d2d623536e11b7773887cb30611d0ecc9
Steffen Kastner <kastner_s@freenet.de>
2022-01-31 13:45:41 +0100

    rotate, scale and translate with two fingers

--- app/src/main/java/de/fhws/indoor/maprenderer/MapView.java
+++ app/src/main/java/de/fhws/indoor/maprenderer/MapView.java
@@ -7,7 +7,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import de.fhws.indoor.radiochecker.R;
@@ -32,17 +31,12 @@ public class MapView extends View {
     private static final int INVALID_POINTER_ID = 1;
     private int mActivePointerId = INVALID_POINTER_ID;
 
-    private float mScaleFactor = 10;
-    private final ScaleGestureDetector mScaleDetector;
+    private final TwoFingerGestureDetector mTwoFingerGestureDetector;
 
-    private float mPosX;
-    private float mPosY;
     private float mLastTouchX;
     private float mLastTouchY;
 
-    private final Matrix mModelTranslate = new Matrix();
-    private final Matrix mModelTranslateInverse = new Matrix();
-    private final Matrix mModelScale = new Matrix();
+    private final Matrix mModelMatrix = new Matrix();
     private final Matrix mViewMatrix = new Matrix();
     private final Matrix mMVMatrix = new Matrix();
     private final Matrix mMVMatrixInverse = new Matrix();
@@ -64,10 +58,9 @@ public class MapView extends View {
         seenPaint.setColor(getResources().getColor(R.color.seenColor, context.getTheme()));
         seenPaint.setTextSize(16);
 
-        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        mTwoFingerGestureDetector = new TwoFingerGestureDetector();
 
-        setPosition(new Vec2(0, 0));
-        setScale(mScaleFactor);
+        mModelMatrix.setScale(10, 10);
     }
 
     public void setMap(Map map) {
@@ -83,20 +76,6 @@ public class MapView extends View {
         if (map == null) { return; }
         floor = map.getFloors().getOrDefault(name, floor);
         invalidate();
-    }
-
-    public void setPosition(Vec2 position) {
-        mPosX = position.x;
-        mPosY = position.y;
-        mModelTranslate.setTranslate(mPosX, mPosY);
-        mModelTranslate.invert(mModelTranslateInverse);
-        updateMVMatrix();
-    }
-
-    public void setScale(float scale) {
-        mScaleFactor = scale;
-        mModelScale.setScale(mScaleFactor, mScaleFactor);
-        updateMVMatrix();
     }
 
     @Override
@@ -119,9 +98,7 @@ public class MapView extends View {
 
     private void updateMVMatrix() {
         mMVMatrix.reset();
-        mMVMatrix.postConcat(mModelTranslateInverse);
-        mMVMatrix.postConcat(mModelScale);
-        mMVMatrix.postConcat(mModelTranslate);
+        mMVMatrix.postConcat(mModelMatrix);
         mMVMatrix.postConcat(mViewMatrix);
         mMVMatrix.invert(mMVMatrixInverse);
     }
@@ -215,7 +192,7 @@ public class MapView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        mScaleDetector.onTouchEvent(ev);
+        if(mTwoFingerGestureDetector.onTouchEvent(ev)) { return true; } // rotation scale detector consumed event
 
         final int action = ev.getAction();
         switch (action & MotionEvent.ACTION_MASK) {
@@ -223,8 +200,11 @@ public class MapView extends View {
                 final float x = ev.getX();
                 final float y = ev.getY();
 
-                mLastTouchX = x;
-                mLastTouchY = y;
+                float[] vec = {x, y};
+                mMVMatrixInverse.mapPoints(vec);
+
+                mLastTouchX = vec[0];
+                mLastTouchY = vec[1];
 
                 // Save the ID of this pointer
                 mActivePointerId = ev.getPointerId(0);
@@ -234,17 +214,22 @@ public class MapView extends View {
             case MotionEvent.ACTION_MOVE: {
                 // Find the index of the active pointer and fetch its position
                 final int pointerIndex = ev.findPointerIndex(mActivePointerId);
-                final float x = ev.getX(pointerIndex);
-                final float y = ev.getY(pointerIndex);
 
-                final float dx = x - mLastTouchX;
-                final float dy = y - mLastTouchY;
-                float[] vec = {dx, dy};
-                mMVMatrixInverse.mapVectors(vec);
-                setPosition(new Vec2(mPosX - vec[0], mPosY - vec[1]));
+                float[] preTransformPoint = {ev.getX(pointerIndex), ev.getY(pointerIndex)};
+                mMVMatrixInverse.mapPoints(preTransformPoint);
 
-                mLastTouchX = x;
-                mLastTouchY = y;
+                final float dx = preTransformPoint[0] - mLastTouchX;
+                final float dy = preTransformPoint[1] - mLastTouchY;
+
+                Matrix offset = new Matrix();
+                offset.postTranslate(dx, dy);
+                mModelMatrix.preConcat(offset);
+                updateMVMatrix();
+
+                float[] postTransformPoint = {ev.getX(pointerIndex), ev.getY(pointerIndex)};
+                mMVMatrixInverse.mapPoints(postTransformPoint);
+                mLastTouchX = postTransformPoint[0];
+                mLastTouchY = postTransformPoint[1];
 
                 invalidate();
                 break;
@@ -264,9 +249,16 @@ public class MapView extends View {
                     // This was our active pointer going up. Choose a new
                     // active pointer and adjust accordingly.
                     final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-                    mLastTouchX = ev.getX(newPointerIndex);
-                    mLastTouchY = ev.getY(newPointerIndex);
+                    float[] vec = {ev.getX(newPointerIndex), ev.getY(newPointerIndex)};
+                    mMVMatrixInverse.mapPoints(vec);
+                    mLastTouchX = vec[0];
+                    mLastTouchY = vec[1];
                     mActivePointerId = ev.getPointerId(newPointerIndex);
+                } else {
+                    float[] vec = {ev.getX(mActivePointerId), ev.getY(mActivePointerId)};
+                    mMVMatrixInverse.mapPoints(vec);
+                    mLastTouchX = vec[0];
+                    mLastTouchY = vec[1];
                 }
                 break;
             }
@@ -274,14 +266,106 @@ public class MapView extends View {
         return true;
     }
 
-    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+    private class TwoFingerGestureDetector {
+        private static final int INVALID_POINTER_ID = -1;
+        private float x1, y1, x2, y2;
+        private int ptrID1 = INVALID_POINTER_ID, ptrID2 = INVALID_POINTER_ID;
 
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            setScale(Math.max(10f, Math.min(mScaleFactor * detector.getScaleFactor(), 500f)));
-            invalidate();
+        public TwoFingerGestureDetector(){
+        }
 
-            return true;
+        public boolean onTouchEvent(MotionEvent event){
+            switch (event.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    ptrID1 = event.getPointerId(event.getActionIndex());
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN: {
+                    ptrID2 = event.getPointerId(event.getActionIndex());
+                    setCurrentFingerState(event);
+                    break;
+                }
+                case MotionEvent.ACTION_MOVE: {
+                    if (ptrID1 != INVALID_POINTER_ID && ptrID2 != INVALID_POINTER_ID) {
+                        float nx2, ny2, nx1, ny1;
+                        nx1 = event.getX(event.findPointerIndex(ptrID1));
+                        ny1 = event.getY(event.findPointerIndex(ptrID1));
+                        nx2 = event.getX(event.findPointerIndex(ptrID2));
+                        ny2 = event.getY(event.findPointerIndex(ptrID2));
+                        float[] vec = {nx1, ny1, nx2, ny2};
+                        mMVMatrixInverse.mapPoints(vec);
+                        nx1 = vec[0];
+                        ny1 = vec[1];
+                        nx2 = vec[2];
+                        ny2 = vec[3];
+                        float cx = (x1 + x2)/2, cy = (y1 + y2)/2;
+                        float ncx = (nx2 + nx1)/2, ncy = (ny2 + ny1)/2;
+
+                        float rotation = angleBetweenLines(x1, y1, x2, y2, nx1, ny1, nx2, ny2);
+                        float scale = scaleBetweenLines(x1, y1, x2, y2, nx1, ny1, nx2, ny2);
+                        float dx = ncx - cx, dy = ncy - cy;
+
+
+                        Matrix offset = new Matrix();
+                        offset.postTranslate(-cx, -cy);
+                        offset.postRotate(rotation);
+                        offset.postScale(scale, scale);
+                        offset.postTranslate(cx, cy);
+                        offset.postTranslate(dx, dy);
+                        mModelMatrix.preConcat(offset);
+                        invalidate();
+
+                        updateMVMatrix();
+                        setCurrentFingerState(event);
+
+                        return true;
+                    }
+                    break;
+                }
+                case MotionEvent.ACTION_UP:
+                    ptrID1 = INVALID_POINTER_ID;
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    ptrID2 = INVALID_POINTER_ID;
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                    ptrID1 = INVALID_POINTER_ID;
+                    ptrID2 = INVALID_POINTER_ID;
+                    break;
+            }
+            return false;
+        }
+
+        private float angleBetweenLines (float fX, float fY, float sX, float sY, float nfX, float nfY, float nsX, float nsY) {
+            float angle1 = (float) Math.atan2( (fY - sY), (fX - sX) );
+            float angle2 = (float) Math.atan2( (nfY - nsY), (nfX - nsX) );
+
+            float angle = ((float)Math.toDegrees(angle2 - angle1)) % 360;
+            if (angle < -180.f) angle += 360.0f;
+            if (angle > 180.f) angle -= 360.0f;
+            return angle;
+        }
+
+        private float scaleBetweenLines(float fX, float fY, float sX, float sY, float nfX, float nfY, float nsX, float nsY) {
+            return lineLength(nfX, nfY, nsX, nsY) / lineLength(fX, fY, sX, sY);
+        }
+
+        private float lineLength(float ox, float oy, float tx, float ty) {
+            float dx = tx - ox;
+            float dy = ty - oy;
+            return (float) Math.sqrt(dx*dx + dy*dy);
+        }
+
+        private void setCurrentFingerState(MotionEvent event) {
+            x1 = event.getX(event.findPointerIndex(ptrID1));
+            y1 = event.getY(event.findPointerIndex(ptrID1));
+            x2 = event.getX(event.findPointerIndex(ptrID2));
+            y2 = event.getY(event.findPointerIndex(ptrID2));
+            float[] vec = {x1, y1, x2, y2};
+            mMVMatrixInverse.mapPoints(vec);
+            x1 = vec[0];
+            y1 = vec[1];
+            x2 = vec[2];
+            y2 = vec[3];
         }
     }
 }
